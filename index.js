287339b9bf54c547262b64c28da57facf8a05e19
guilherme sts gomes <guistsgomes07@gmail.com>
2023-11-25 18:14:13 -0300

    instalação do hendleabars e nodemon

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const exphbs = require("express-handLeabars")
 
 const app = express()
+app.engine('hendLeabars', exphbs())
+app.set("view engine", 'hendleabars')
 
 app.get('/', (requisicao,resposta) => {
     resposta.send("ola mundo")
