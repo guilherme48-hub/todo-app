32f8779ea70495b007003c715798047b8f7f7c94
guilherme sts gomes <guistsgomes07@gmail.com>
2023-11-29 20:57:07 -0300

    salvando tarefa

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const mysql = require("mysql2")
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql2");
 
-const app = express()
+const app = express();
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', (requisicao,resposta) => {
-    resposta.render('home')
-})
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao;
+    const completa = 0;
+
+    const sql = `
+    INSERT INTO tarefas(descricao, completa)
+    VALUES ('${descricao}', '${completa}')
+    `;
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro);
+        }
+
+        resposta.redirect('/');
+    });
+});
+
+app.get('/', (requisicao, resposta) => {
+    resposta.render('home');
+});
 
 const conexao = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "todoapp", 
+    database: "todoapp",
     port: 3306
 });
 
+
+
 conexao.connect((erro) => {
     if (erro) {
-        return console.log(erro); 
+        return console.log(erro);
     }
 
     console.log("Estou conectado com MySQL");
